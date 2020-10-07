57d231ddf032d9304867f73ad0508936383228bd
Maria Ananickal <mariajoe@outlook.com>
2020-10-06 20:20:57 -0700

    Commit with JavaDoc comments added

--- cse360_Assignment2/src/cse360assignment02/AddingMachine.java
+++ cse360_Assignment2/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,54 @@
 package cse360assignment02;
 
+/**
+ * @author Maria Ananickal
+ * @version 1.1
+ */
 public class AddingMachine {
 	  private int total;
 	  
+	  /**
+	   * constructor for the AddingMachine class
+	   * creates AddingMachine object
+	   */
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
 	  }
-	  
+
+	  /**
+	   * returns the total 
+	   * @return the current total
+	   */
 	  public int getTotal () {
 	    return 0;
 	  }
 	  
+	  /**
+	   * Adds the value passed in to the total
+	   * @param integer value to be added
+	   */
 	  public void add (int value) {
 	  }
 
+	  /**
+	   * Subtracts the value passed in from the total
+	   * @param integer value to be subtracted
+	   */
 	  public void subtract (int value) {
 	  }
 
+	  /**
+	   * Creates string of all values and methods that have been called 
+	   * @return string of values passed into previous methods
+	   */
 	  public String toString () {
 	    return "";
 	  }
 
+	  /**
+	   * clears total and sets it back to zero 
+	   */
 	  public void clear() {
 	  }
+	  
 }
